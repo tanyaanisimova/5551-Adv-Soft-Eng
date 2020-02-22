cddef88f9ba203508a8941c289b5db7af6c289f0
Anik Paul Gomes <gomesap29@gmail.com>
2020-02-21 21:21:46 -0600

    Ui designed

--- lab 4/source/authApp/src/app/tab1/tab1.page.ts
+++ lab 4/source/authApp/src/app/tab1/tab1.page.ts
@@ -1,18 +1,56 @@
-import { Component } from '@angular/core';
-import {Router} from '@angular/router';
-import {AuthService} from '../auth/auth.service';
+import { Component } from "@angular/core";
+import { HttpService } from "./http.service";
+import { AlertController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"]
 })
 export class Tab1Page {
+  firstName: string = "";
+  gender: string = "";
+  samples: number = 0;
+  accuracy: number = 0;
+  showOutput: boolean = false;
 
-  constructor( private router: Router, private authService: AuthService) {}
+  constructor(
+    private httpService: HttpService,
+    private alertController: AlertController
+  ) {}
+
+  async presentAlert(message: string) {
+    const alert = await this.alertController.create({
+      //header: header,
+      message: message,
+      buttons: ["OK"]
+    });
+
+    await alert.present();
+  }
 
   logout() {
     this.authService.logout();
-    this.router.navigateByUrl('login');
+    this.router.navigateByUrl("login");
+  }
+
+  getGender(): void {
+    if (this.firstName == "") {
+      this.presentAlert("Enter a name to view results.");
+      return;
+    }
+    this.httpService.getGenderData(this.firstName).subscribe(data => {
+      this.gender = data["gender"];
+      this.samples = data["samples"];
+      this.accuracy = data["accuracy"];
+      this.showOutput = true;
+    });
+  }
+
+  getGenderByFristName(name: string): string {
+    this.httpService.getGenderData(this.firstName).subscribe(data => {
+      return data["gender"];
+    });
+    return;
   }
 }
