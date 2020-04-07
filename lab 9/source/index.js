3b54b0fb0e0c7bcf94a2f0e8f6cadfae64af6973
Tanya Anisimova <anisimova.tanya07@gmail.com>
2020-04-06 21:55:38 -0500

    refactoring

--- lab 9/source/index.js
+++ lab 9/source/index.js
@@ -12,11 +12,6 @@ app.get('/readme', (req, res) => {
     res.json({ "message" : "This is open to the world!" })
 })
 
-// private: only allow authorized users
-app.get('/secret', isAuthorized, (req, res) => {
-    res.json({ "message" : "THIS IS SUPER SECRET, DO NOT SHARE!" })
-})
-
 // print JWT signed with the private key file
 app.get('/jwt', (req, res) => {
     let privateKey = fs.readFileSync('./private.pem', 'utf8');
@@ -24,17 +19,22 @@ app.get('/jwt', (req, res) => {
     res.send(token);
 })
 
+// private: only allow authenticated users
+app.get('/secret', isAuthenticated, (req, res) => {
+    res.json({ "message" : "THIS IS SUPER SECRET, DO NOT SHARE!" })
+})
+
 // start application on the specified port
-app.listen(port, 
+app.listen(port,
     () => console.log(`Simple Express app listening on port ${port}!`))
 
 
 // verify that the token provided was signed by our private key file
-function isAuthorized(req, res, next) {
+function isAuthenticated(req, res, next) {
     if (typeof req.headers.authorization !== "undefined") {
         let token = req.headers.authorization.split(" ")[0];
         let privateKey = fs.readFileSync('./private.pem', 'utf8');
-        
+
         jwt.verify(token, privateKey, { algorithm: "HS256" }, (err, user) => {
             if (err) {  // not signed by our private key file
                 res.status(500).json({ error: "Not Authorized" });
